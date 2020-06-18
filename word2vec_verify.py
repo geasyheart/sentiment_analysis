a204ff4cb8ed961f08283694d38c9808db22e88a
yuzhang <zhangy@lianzhu.com>
2020-06-18 08:59:43 +0800

    :hammer: 加载另外一种格式

--- word2vec_verify.py
+++ word2vec_verify.py
@@ -1,4 +1,5 @@
 import gensim
+from gensim.models import KeyedVectors
 
 model = gensim.models.Word2Vec.load('data/simple.zh.text.model')
 
@@ -7,3 +8,9 @@ result = model.most_similar('男朋友', topn=10)
 for each in result:
     print(each)
 
+print('2'.center(50, '-'))
+model2 = KeyedVectors.load_word2vec_format('data/simple.zh.text.vector')
+result = model2.most_similar('男朋友', topn=10)
+
+for each in result:
+    print(each)
